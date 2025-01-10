0ab3c3530e835d50b8cafc4782861f87a92660be
Aditya Garg <140079562+adityagarg023@users.noreply.github.com>
2025-01-11 03:13:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// scripts.js
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+});
+
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function downloadResume() {
+    const url = 'path/to/resume.pdf';
+    fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = '/resume.docx';
+            link.click();
+        });
+}
